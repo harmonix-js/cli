826fb8e937d00b347e19be1d660517c940c5be33
nethris <david.abou@epitech.eu>
2024-05-03 09:13:28 -0400

    feat: downloading template on init command

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,11 +1,28 @@
 import { defineCommand } from 'citty'
+import { downloadTemplate } from 'giget'
+import { consola } from 'consola'
 
 export default defineCommand({
 	meta: {
 		name: 'init',
 		description: 'Initialize a new Harmony project'
 	},
+	args: {
+		dir: {
+			type: 'positional',
+			description: 'Project directory',
+			default: 'harmony-starter'
+		}
+	},
 	async run(ctx) {
-		console.log('Hello, World!', ctx.cmd)
+		await downloadTemplate('github:harmony-ts/starter', {
+			dir: ctx.args.dir,
+			cwd: '.'
+		})
+
+		consola.log(`✨ Harmony project has been created. Next steps:`)
+		consola.log(` › cd ${ctx.args.dir}`)
+		consola.log(' › npm install')
+		consola.log(' › npm run dev')
 	}
 })
