9702cbc9991bf724812a4348e108f7d3d6509e43
nethris <david.abou@epitech.eu>
2024-05-09 22:41:43 -0400

    feat: auto installing deps

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,6 +1,7 @@
 import { defineCommand } from 'citty'
 import { downloadTemplate } from 'giget'
 import { consola } from 'consola'
+import { installDependencies, type PackageManagerName } from 'nypm'
 
 export default defineCommand({
   meta: {
@@ -15,14 +16,37 @@ export default defineCommand({
     }
   },
   async run(ctx) {
-    await downloadTemplate('github:harmonix-js/starter', {
+    const template = await downloadTemplate('github:harmonix-js/starter', {
       dir: ctx.args.dir,
       cwd: '.'
     })
+		const packageManagerOptions: PackageManagerName[] = [
+			'bun',
+			'npm',
+			'pnpm',
+			'yarn'
+		]
+		const selectedPackageManager = await consola.prompt('Wich package manager would you like to use?', {
+			type: 'select',
+			options: packageManagerOptions
+		})
 
+		consola.start('Installing dependencies...')
+		try {
+			await installDependencies({
+				cwd: template.dir,
+				packageManager: {
+					name: selectedPackageManager,
+					command: selectedPackageManager
+				}
+			})
+		} catch (err) {
+			consola.error((err as Error).toString())
+			process.exit(1)
+		}
+		consola.success('Installation completed.')
     consola.log(`✨ Harmonix project has been created. Next steps:`)
     consola.log(` › cd ${ctx.args.dir}`)
-    consola.log(' › npm install')
     consola.log(' › npm run start')
   }
 })
